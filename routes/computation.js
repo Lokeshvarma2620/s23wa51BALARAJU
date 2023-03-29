95aa0d0789f4937ccb8cc44c3797870e1467fc05
Lokeshvarma2620 <Lokeshvarma1014@gmail.com>
2023-03-29 15:50:38 -0500

    updated

--- routes/computation.js
+++ routes/computation.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var ran=Math.random()*100;
+var randomvalue=Math.random()*100;
 var x=0;
 var y=0;
 var z=0;
-var a=0;
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  x=Math.pow(ran,2);
-  y=Math.sign(ran);
-  z=Math.tanh(ran);
-  a=Math.trunc(ran);
+  x=Math.imul(randomvalue,2);
+  y=Math.log(randomvalue);
+  z=Math.log10(randomvalue);
+  
 
-  res.send(`pow function applied to ${ran} is ${x}, <br> sign function applied to ${ran} is ${y}, <br> tanh function applied to ${ran} is ${z} <br> trunc function applied to ${ran} is ${a}`);
+  res.send(`imul function applied to ${randomvalue} is ${x}, <br> log function applied to ${randomvalue} is ${y}, <br> log10 function applied to ${randomvalue} is ${z}`);
 });
 
 module.exports = router;
